5e60f29f5b3228d4b98dd35fb25762e9235e289c
A O <alex.oshchepkov@gmail.com>
2020-07-09 13:24:14 +0500

    update to ts@next

--- out/file-with-comment.js
+++ out/file-with-comment.js
@@ -1,4 +1,5 @@
 "use strict";
+/* @some-comment */
 Object.defineProperty(exports, "__esModule", { value: true });
 var mod_1 = require("./mod");
 console.log(mod_1.default);
--- out/file-with-comment2.js
+++ out/file-with-comment2.js
@@ -1,3 +1,4 @@
 "use strict";
+// @some-comment
 Object.defineProperty(exports, "__esModule", { value: true });
 var a = {};
